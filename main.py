9232a8ddd427989b47867d16b8832bd132f286c6
Nitin Rawat <40258192+nitinrawat111@users.noreply.github.com>
2022-02-20 10:52:58 +0000

    Added support for fetching multiple memes(Max 50)

--- main.py
+++ main.py
@@ -4,22 +4,35 @@ import os
 
 
 client = discord.Client()
-API_LINK = "https://meme-api.herokuapp.com/gimme"
+API_LINK = "https://meme-api.herokuapp.com/gimme/"
 
 
-def getJsonFromAPI():
-  requestData = requests.get(API_LINK)
+def getJsonFromAPI(memesCount):
+  finalUrl = API_LINK + str(memesCount)
+  requestData = requests.get(finalUrl)
   jsonData = requestData.json()
   return jsonData
 
 
-def getRandomMemeUrl():
+def getRandomMemesUrls(memesCount):
   try:
-    jsonData = getJsonFromAPI()
-    randomMemeURL = jsonData["url"]
-    return randomMemeURL
+    if(memesCount > 50):
+      memesCount = 50
+    jsonData = getJsonFromAPI(memesCount)
+    memesList = jsonData["memes"]
+    
+    randomMemesUrls = []
+    for i in range(memesCount):
+      randomMemesUrls.append( memesList[i]["url"] )
+    
+    return randomMemesUrls
   except:
-    return "```\nRan into a problem :(\nPlease try again Later..```"
+    return ["```\nRan into a problem :(\nPlease try again Later..```"]
+
+
+async def sendMultipleMessages(messageList, channel):
+  for eachMessage in messageList:
+    await channel.send(eachMessage)
 
 
 @client.event
@@ -29,8 +42,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  if(message.author == client.user):
+    return
+
   if(message.content == "$meme"):
-    randomMemeURL = getRandomMemeUrl()
-    await message.channel.send(randomMemeURL)
+    memesUrls = getRandomMemesUrls(memesCount = 1)
+    await sendMultipleMessages(memesUrls, message.channel)
+
+  if(message.content.startswith("$meme ")):
+    if(message.content[6:].isdigit()):
+      memesCount = int(message.content[6:])
+      memesUrls = getRandomMemesUrls(memesCount)
+      await sendMultipleMessages(memesUrls, message.channel)
+
 
 client.run(os.environ['TOKEN'])
